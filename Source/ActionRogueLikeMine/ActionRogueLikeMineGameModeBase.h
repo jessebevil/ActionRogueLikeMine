ae48fbcbad4e46b1bbce3e6d6f13ec02ac30b077
Jesse <Jesse.m.bevil.mil@gmail.com>
2022-03-24 11:04:13 -0500

    Initial Project creation

---
+++ Source/ActionRogueLikeMine/ActionRogueLikeMineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueLikeMineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKEMINE_API AActionRogueLikeMineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
