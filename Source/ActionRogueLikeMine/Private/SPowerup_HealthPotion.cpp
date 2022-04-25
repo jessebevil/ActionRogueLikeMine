916cef2051e978bdf6be68e54ac9752a0e086c55
Jesse <Jesse.m.bevil.mil@gmail.com>
2022-04-25 03:00:33 -0500

    Create base class for Powerup, and add HealthPotion class

---
+++ Source/ActionRogueLikeMine/Private/SPowerup_HealthPotion.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPowerup_HealthPotion.h"
+#include "AttributeComponent.h"
+
+ASPowerup_HealthPotion::ASPowerup_HealthPotion() {
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	MeshComp->SetupAttachment(RootComponent);
+}
+
+void ASPowerup_HealthPotion::Interact_Implementation(APawn* InstigatorPawn) {
+	if (!ensure(InstigatorPawn)) {
+		return;
+	}
+
+	UAttributeComponent* AttributeComp = Cast<UAttributeComponent>(InstigatorPawn->GetComponentByClass(UAttributeComponent::StaticClass()));
+	// Check if not already at max health
+	if (ensure(AttributeComp) && !AttributeComp->IsFullHealth()) {
+		// Only activate if healed successfully
+		if (AttributeComp->ApplyHealthChange(AttributeComp->GetHealthMax())) {
+			HideAndCooldownPowerup();
+		}
+	}
+}
---
+++ Source/ActionRogueLikeMine/Public/SPowerupActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SGameplayInterface.h"
+#include "SPowerupActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class ACTIONROGUELIKEMINE_API ASPowerupActor : public AActor, public ISGameplayInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASPowerupActor();
+	void Interact_Implementation(APawn* IntigatorPawn);
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Powerup")
+	float RespawnTime;
+
+	FTimerHandle TimerHandle_RespawnTimer;
+
+	UFUNCTION()
+	void ShowPowerup();
+
+	void HideAndCooldownPowerup();
+
+	void SetPowerupState(bool bNewIsActive);
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* SphereComp;
+};
